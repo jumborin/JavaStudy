28d4537fba9874c5d95d7af8b22453f6afe134ee
jumborin <y.f.architect@gmail.com>
2019-03-31 18:11:45 +0900

    PDFBoxを利用してテキストだけのPDFファイルを作成するメソッドを作成

---
+++ src/useFile/UsePdfFile.java
@@ -0,0 +1,58 @@
+/**
+ *
+ */
+package useFile;
+
+import java.io.IOException;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+
+/**
+ * PDFファイルを扱うクラス。
+ * 利用条件：PDFBoxが必要。
+ * @author jumborin
+ *
+ */
+public class UsePdfFile extends UseFile {
+
+	/**
+	 * 空のPDFファイルを作成する。
+	 * 利用条件：PDFBoxが必要。
+	 *
+	 * @param pdfFileName PDFファイル名
+	 * @param outputText 出力するテキスト
+	 * @throws IOException
+	 */
+	public void createPdfFileOnlyText(final String pdfFileName, final String outputText) throws IOException {
+		// ドキュメントオブジェクトの作成
+		PDDocument document = new PDDocument();
+
+		// ページオブジェクトの作成
+		PDPage page = new PDPage();
+		document.addPage(page);
+
+		//文字出力処理
+		PDPageContentStream contentStream = new PDPageContentStream(document, page);
+		contentStream.beginText();
+
+		//フォント指定
+		PDFont font = PDType1Font.TIMES_ITALIC;
+		contentStream.setFont(font, 12);
+
+		//出力位置指定
+		contentStream.newLineAtOffset(0f, 0f);
+
+		//出力文字列
+		contentStream.showText(outputText);
+		contentStream.endText();
+		contentStream.close();
+
+		// ドキュメントを保存します
+		document.save(pdfFileName);
+		document.close();
+	}
+}
---
+++ test/useFileTest/UsePdfFileTest.java
@@ -0,0 +1,63 @@
+package useFileTest;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import cons.TestConst;
+import useFile.UsePdfFile;
+
+class UsePdfFileTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	/**
+	 * createPdfFileOnlyText()メソッドのテストメソッド
+	 */
+	@Test
+	void testCreatePdfFileOnlyText() {
+
+		// テスト出力するPDFファイル名
+		String testPdfFile = TestConst.OUTPUT_DATA_DIR + "test.pdf";
+
+		// テスト対象のクラスをインスタンス化
+		UsePdfFile upf = new UsePdfFile();
+
+		try {
+			// テスト対象のメソッドを実行
+			upf.createPdfFileOnlyText(testPdfFile, "test message");
+
+			// PDFファイルが存在することを確認
+			assertEquals(true, upf.isExistFile(testPdfFile));
+
+			// PDFファイルを削除できたことを確認
+			assertEquals(true, upf.deleteFile(testPdfFile));
+
+			// PDFファイルが存在しないことを確認
+			assertEquals(false, upf.isExistFile(testPdfFile));
+
+		} catch (IOException e) {
+			fail("IOExceptionでエラー");
+		}
+	}
+}
