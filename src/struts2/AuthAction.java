875ceee1ac6fee1d03306eb3d826ab38f821ff52
user <y.f.architect@gmail.com>
2019-11-16 17:19:57 +0900

    struts2勉強用

---
+++ src/struts2/AuthAction.java
@@ -0,0 +1,21 @@
+/**
+ *
+ */
+package struts2;
+
+import com.opensymphony.xwork2.Action;
+
+/**
+ * ログイン処理ログアウト処理アクション
+ * @author user
+ *
+ */
+public class AuthAction implements Action {
+
+	@Override
+	public String execute() throws Exception {
+		// TODO 自動生成されたメソッド・スタブ
+		return "success";
+	}
+
+}
