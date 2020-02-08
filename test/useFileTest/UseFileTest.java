141bcc02c653fcffe454d44823d839bd5d9b6627
Jumborin <y.f.architect@gmail.com>
2020-02-08 21:39:11 +0900

    assert文の想定値が誤っていたため修正

--- test/useFileTest/UseFileTest.java
+++ test/useFileTest/UseFileTest.java
@@ -1,6 +1,5 @@
 package useFileTest;
 
-
 import static org.junit.Assert.assertEquals;
 
 import java.util.List;
@@ -21,67 +20,67 @@ import useFile.UseFile;
  */
 class UseFileTest {
 
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-	}
+    @BeforeAll
+    static void setUpBeforeClass() throws Exception {
+    }
 
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
-	}
+    @AfterAll
+    static void tearDownAfterClass() throws Exception {
+    }
 
-	@BeforeEach
-	void setUp() throws Exception {
-	}
+    @BeforeEach
+    void setUp() throws Exception {
+    }
 
-	@AfterEach
-	void tearDown() throws Exception {
-	}
+    @AfterEach
+    void tearDown() throws Exception {
+    }
 
-	/**
-	 * isExistFile()メソッドのテストメソッド
-	 */
-	@Test
-	void testIsExistFile() {
+    /**
+     * isExistFile()メソッドのテストメソッド
+     */
+    @Test
+    void testIsExistFile() {
 
-		// 読み込み対象のファイル
-		final String testDataFile = TestConst.INPUT_DATA_DIR + "java.properties";
-		UseFile uf = new UseFile();
+	// 読み込み対象のファイル
+	final String testDataFile = TestConst.INPUT_DATA_DIR + "java.properties";
+	UseFile uf = new UseFile();
 
-		// ファイルが存在することを確認
-		assertEquals(true, uf.isExistFile(testDataFile));
+	// ファイルが存在することを確認
+	assertEquals(true, uf.isExistFile(testDataFile));
 
-		// ファイルが削除できないことを確認
-		assertEquals(false, uf.deleteFile("NOT_EXIST.FILE"));
-	}
+	// ファイルが削除できないことを確認
+	assertEquals(false, uf.deleteFile("NOT_EXIST.FILE"));
+    }
 
-	@Test
-	void testDeleteFile() {
-		UseFile uf = new UseFile();
-		// ファイルが削除できないことを確認
-		assertEquals(false, uf.deleteFile("NOT_EXIST.FILE"));
-	}
+    @Test
+    void testDeleteFile() {
+	UseFile uf = new UseFile();
+	// ファイルが削除できないことを確認
+	assertEquals(false, uf.deleteFile("NOT_EXIST.FILE"));
+    }
 
-	/**
-	 * getFileNameList()メソッドのテストメソッド
-	 */
-	@Test
-	void testGetFileNameList() {
-		UseFile uf = new UseFile();
-		List<String> list = uf.getFileNameList(TestConst.INPUT_DATA_DIR);
-		assertEquals(7, list.size());
-		assertEquals(false, list.contains(TestConst.INPUT_DATA_DIR + "package-info.java"));
-		assertEquals(true, list.contains("package-info.java"));
-	}
+    /**
+     * getFileNameList()メソッドのテストメソッド
+     */
+    @Test
+    void testGetFileNameList() {
+	UseFile uf = new UseFile();
+	List<String> list = uf.getFileNameList(TestConst.INPUT_DATA_DIR);
+	assertEquals(9, list.size());
+	assertEquals(false, list.contains(TestConst.INPUT_DATA_DIR + "package-info.java"));
+	assertEquals(true, list.contains("package-info.java"));
+    }
 
-	/**
-	 * getFullFilePathList()メソッドのテストメソッド
-	 */
-	@Test
-	void testGetFullFilePathList() {
-		UseFile uf = new UseFile();
-		List<String> list = uf.getFullFilePathList(TestConst.INPUT_DATA_DIR);
-		assertEquals(7, list.size());
-		assertEquals(true, list.contains(TestConst.INPUT_DATA_DIR + "package-info.java"));
-		assertEquals(false, list.contains("package-info.java"));
-	}
+    /**
+     * getFullFilePathList()メソッドのテストメソッド
+     */
+    @Test
+    void testGetFullFilePathList() {
+	UseFile uf = new UseFile();
+	List<String> list = uf.getFullFilePathList(TestConst.INPUT_DATA_DIR);
+	assertEquals(9, list.size());
+	assertEquals(true, list.contains(TestConst.INPUT_DATA_DIR + "package-info.java"));
+	assertEquals(false, list.contains("package-info.java"));
+    }
 }
