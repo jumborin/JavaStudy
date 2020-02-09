3ead54420c9c801d85691486cdeafc2c06a57b74
Jumborin <y.f.architect@gmail.com>
2020-02-09 10:05:09 +0900

    import文を少し修正

--- test/daoTest/H2DatabaseDaoTest.java
+++ test/daoTest/H2DatabaseDaoTest.java
@@ -1,6 +1,7 @@
 package daoTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -18,60 +19,60 @@ import dbunitUtil.DbUnitUtil;
 
 class H2DatabaseDaoTest {
 
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-	}
+    @BeforeAll
+    static void setUpBeforeClass() throws Exception {
+    }
 
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
-	}
+    @AfterAll
+    static void tearDownAfterClass() throws Exception {
+    }
 
-	@BeforeEach
-	void setUp() throws Exception {
-	}
+    @BeforeEach
+    void setUp() throws Exception {
+    }
 
-	@AfterEach
-	void tearDown() throws Exception {
-	}
+    @AfterEach
+    void tearDown() throws Exception {
+    }
 
-	@Test
-	void testSelect() {
-		try {
-			List<Map<String, String>> list = H2DatabaseDao.select("select * from test;");
-			Map<String, String> map = list.get(0);
-			assertEquals("1", map.get("ID"));
-			assertEquals("tarou", map.get("NAME"));
-			map = list.get(1);
-			assertEquals("2", map.get("ID"));
-			assertEquals("jirou", map.get("NAME"));
-		} catch (ClassNotFoundException e) {
-			fail("ClassNotFoundExceptionでエラー");
-		} catch (SQLException e) {
-			fail("SQLExceptionでエラー");
-		} catch (Exception e) {
-			fail("Exceptionでエラー");
-		}
+    @Test
+    void testSelect() {
+	try {
+	    List<Map<String, String>> list = H2DatabaseDao.select("select * from test;");
+	    Map<String, String> map = list.get(0);
+	    assertEquals("1", map.get("ID"));
+	    assertEquals("tarou", map.get("NAME"));
+	    map = list.get(1);
+	    assertEquals("2", map.get("ID"));
+	    assertEquals("jirou", map.get("NAME"));
+	} catch (ClassNotFoundException e) {
+	    fail("ClassNotFoundExceptionでエラー");
+	} catch (SQLException e) {
+	    fail("SQLExceptionでエラー");
+	} catch (Exception e) {
+	    fail("Exceptionでエラー");
 	}
+    }
 
-	@Test
-	void testSelectDbUnit() {
-		try {
-			DbUnitUtil.setUpNonExistSchema(TestConst.INPUT_DATA_DIR + "InitTestTable.xml");
-			List<Map<String, String>> list = H2DatabaseDao.select("select * from test;");
-			Map<String, String> map = list.get(0);
-			assertEquals("1", map.get("ID"));
-			assertEquals("tarou", map.get("NAME"));
-			map = list.get(1);
-			assertEquals("2", map.get("ID"));
-			assertEquals("jirou", map.get("NAME"));
-			DbUnitUtil.check(TestConst.INPUT_DATA_DIR + "ExpectedTestTable.xml", "test", new String[0]);
-			DbUnitUtil.tearDown();
-		} catch (ClassNotFoundException e) {
-			fail("ClassNotFoundExceptionでエラー");
-		} catch (SQLException e) {
-			fail("SQLExceptionでエラー");
-		} catch (Exception e) {
-			fail("Exceptionでエラー");
-		}
+    @Test
+    void testSelectDbUnit() {
+	try {
+	    DbUnitUtil.setUpNonExistSchema(TestConst.INPUT_DATA_DIR + "InitTestTable.xml");
+	    List<Map<String, String>> list = H2DatabaseDao.select("select * from test;");
+	    Map<String, String> map = list.get(0);
+	    assertEquals("1", map.get("ID"));
+	    assertEquals("tarou", map.get("NAME"));
+	    map = list.get(1);
+	    assertEquals("2", map.get("ID"));
+	    assertEquals("jirou", map.get("NAME"));
+	    DbUnitUtil.check(TestConst.INPUT_DATA_DIR + "ExpectedTestTable.xml", "test", new String[0]);
+	    DbUnitUtil.tearDown();
+	} catch (ClassNotFoundException e) {
+	    fail("ClassNotFoundExceptionでエラー");
+	} catch (SQLException e) {
+	    fail("SQLExceptionでエラー");
+	} catch (Exception e) {
+	    fail("Exceptionでエラー");
 	}
+    }
 }
